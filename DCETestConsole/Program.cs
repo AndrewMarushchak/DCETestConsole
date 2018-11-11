8f3879b36ee3eecafde4ce0a7bc9c0a3e1947005
Andrey <Andrew.Marushchak@gmail.com>
2018-11-11 17:29:06 +0200

    RestSharp test code for Okoin.com

--- DCETestConsole/Program.cs
+++ DCETestConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace DCETestConsole
 {
@@ -10,6 +11,18 @@ namespace DCETestConsole
     {
         static void Main(string[] args)
         {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://www.okcoin.com/api/v1");
+
+            var request = new RestRequest();
+            request.AddHeader("contentType", "application/x-www-form-urlencoded");
+            request.Resource = "ticker.do?symbol=btc_usd";
+
+            IRestResponse response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+
+            Console.ReadKey();
         }
     }
 }
