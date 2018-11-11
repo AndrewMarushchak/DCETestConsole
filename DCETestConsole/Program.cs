f3810e91d8cbf7d321e3b54c3e114d360e59fc2e
Andrey <Andrew.Marushchak@gmail.com>
2018-11-11 22:45:17 +0200

    WebSocket4net test code for Okcoin.com

--- DCETestConsole/Program.cs
+++ DCETestConsole/Program.cs
@@ -11,18 +11,34 @@ namespace DCETestConsole
     {
         static void Main(string[] args)
         {
-            var client = new RestClient();
-            client.BaseUrl = new Uri("https://www.okcoin.com/api/v1");
+            //var client = new RestClient();
+            //client.BaseUrl = new Uri("https://www.okcoin.com/api/v1");
 
-            var request = new RestRequest();
-            request.AddHeader("contentType", "application/x-www-form-urlencoded");
-            request.Resource = "ticker.do?symbol=btc_usd";
+            //var request = new RestRequest();
+            //request.AddHeader("contentType", "application/x-www-form-urlencoded");
+            //request.Resource = "ticker.do?symbol=btc_usd";
 
-            IRestResponse response = client.Execute(request);
+            //IRestResponse response = client.Execute(request);
 
-            Console.WriteLine(response.Content);
+            //Console.WriteLine(response.Content);
+
+            string url = "wss://real.okcoin.com:10440/websocket/okcoinapi";
+
+            WebSocketManager socketManager = new WebSocketManager(url, new BuissnesProcessor());
+
+            socketManager.Start();
+            socketManager.Send("{'event':'addChannel','channel':'ok_sub_spotcny_btc_ticker'}");
+            socketManager.Stop();
 
             Console.ReadKey();
+        }
+    }
+
+    public class BuissnesProcessor : IWebSocketDataProcessor
+    {
+        public void ProcessData(string data)
+        {
+            Console.WriteLine(data);
         }
     }
 }
---
+++ DCETestConsole/WebSocketManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocket4Net;
+using SuperSocket.ClientEngine;
+
+namespace DCETestConsole
+{
+    public interface IWebSocketDataProcessor
+    {
+        void ProcessData(string data);
+    }
+
+    public class WebSocketManager
+    {
+        private WebSocket webSocket;
+        private IWebSocketDataProcessor dataProcessor;
+
+        public WebSocketManager(string webSocketUrl, IWebSocketDataProcessor dataProcessor)
+        {
+            if (webSocketUrl == null || dataProcessor == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.dataProcessor = dataProcessor;
+            webSocket = new WebSocket(webSocketUrl);
+            
+            webSocket.MessageReceived += new EventHandler<MessageReceivedEventArgs>(Websocket_MessageReceived);
+        }
+
+        public void Start()
+        {
+            webSocket.Open();
+
+            while (webSocket.State == WebSocketState.Connecting)
+            {
+                // by default webSocket4Net has AutoSendPing=true, so we need to wait until connection established
+            }
+
+            if (webSocket.State != WebSocketState.Open)
+            {
+                throw new ApplicationException($"Connection is not opened: {webSocket}");
+            }
+        }
+
+        public void Stop()
+        {
+            if (webSocket != null)
+            {
+                webSocket.Close();
+            }                
+        }
+
+        public void Send(string message)
+        {
+            webSocket.Send(message);
+        }
+
+        private void Websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            dataProcessor.ProcessData(e.Message);
+        }
+    }
+}
